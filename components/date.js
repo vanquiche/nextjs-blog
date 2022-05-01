290e8e4245a6d5d9b5f99911a38915de976afceb
vanquiche <stevevang2010@hotmail.com>
2022-05-01 18:02:36 +0900

    initial commit

---
+++ components/date.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { parseISO, format } from 'date-fns';
+
+const Date = ({ dateString }) => {
+  const date = parseISO(dateString);
+
+  return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>;
+};
+
+export default Date;
